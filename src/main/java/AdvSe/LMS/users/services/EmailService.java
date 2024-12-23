388a22b6c56913d97f11dbd95ef4493c6bc6f716
y0riii <70681103+y0riii@users.noreply.github.com>
2024-12-23 15:06:44 +0200

    handling errors in emails

--- src/main/java/AdvSe/LMS/users/services/EmailService.java
+++ src/main/java/AdvSe/LMS/users/services/EmailService.java
@@ -1,6 +1,7 @@
 package AdvSe.LMS.users.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,17 @@ public class EmailService {
     private JavaMailSender mailSender;
 
     public void sendEmail(String to, String subject, String body) {
-    	if(to == null || to.isEmpty()) return;
+        if (to == null || to.isEmpty()) return;
+
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
-        mailSender.send(message);
+
+        try {
+            mailSender.send(message);
+        } catch (MailException e) {
+            System.err.println("Error sending email to: " + to + " - " + e.getMessage());
+        }
     }
 }
